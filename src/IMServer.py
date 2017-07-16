e4a63771ce3b9c8e1cb48e6c89c9a71e5c752896
Whitney Dluhosh <whitneydluhosh@hotmail.com>
2017-07-16 16:04:26 -0700

    implemented createChatRoom in IMServer

--- src/IMServer.py
+++ src/IMServer.py
@@ -2,13 +2,22 @@
 
 # Loops, listening for new IMClients wanting to enter a ChatRoom
 # Keep a list of Client objects for each IMClient and a list for existing ChatRooms
+
 class IMServer: 
 
 	def __init__(self):
 		self.chatRooms = []
 		self.connectedClients = []
-
+		
+		
 	def createChatRoom(self, roomName):
+		general = ChatRoom()
+		general.name = "General"
+		self.chatRooms.append(general)
+		random = ChatRoom()
+		random.name = "Random"
+		self.chatRooms.append(random)
+		
 		raise NotImplementedError
 	
 	def listenForConnections(self):
