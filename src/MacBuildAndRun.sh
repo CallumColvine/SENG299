d9b68d0e201aec40d2a0c1278d6ac4d9301f833c
Rafay <rafay_chaudhry@live.com>
2017-07-19 12:50:27 -0700

    Just changed MacBuild file

--- src/MacBuildAndRun.sh
+++ src/MacBuildAndRun.sh
@@ -2,7 +2,7 @@
 osascript -e 'tell application "Terminal" to activate' 
 osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' 
 osascript -e 'tell application "Terminal" to do script "python /Users/academia/Desktop/peanut/src/IMServer.py" in selected tab of the front window'
-for VARIABLE in {1..150}
+for VARIABLE in {1..30}
 	do
 		osascript -e 'tell application "Terminal" to activate' 
 		osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' 
